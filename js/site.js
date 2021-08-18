11ff66d18a59b4305caf9fbf7f794ae99cc12260
patriciosebastian <psebastiansalazar@gmail.com>
2021-08-18 00:05:58 -0700

    planning the JS with pseudocode

--- js/site.js
+++ js/site.js
@@ -1,4 +1,39 @@
-// Call Hello World
-function HelloWorld() {
-    alert('Hello World');
+// Get values from the user. We need to get the fizz and the buzz value.
+function getValues() {
+    // get the user values from the page
+
+    // parse for numbers
+
+    // check to make sure numbers are integers
+
+    // we call fizzBuzz
+
+    // call displayData and write the values to the screen
+}
+
+function fizzBuzz(fizzValue, buzzValue) {
+    // init the returnArray
+    let returnArray = [];
+
+    // loop from 1 to 100
+
+    // we need to check the current value in three steps
+    // check to see if divisible by both (3 and 5)
+    // if so, push 'FizzBuzz' into an array and not the number
+
+    // check to see if divisible by fizz value (3)
+        // if so, push 'Fizz' into an array and not the number
+
+    // check to see if divisible by buzz value (5)
+    // if so, push 'Buzz' into an array and not the number
+
+    // if none, then push the number into the array
+
+    returnArray;
+}
+
+function displayData(fbArray) {
+    // loop over the array and create a table row (<tr>) for each item.
+
+    // add all the rows to the table.
 }
